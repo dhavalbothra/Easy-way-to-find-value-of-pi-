e63bcdc1f267b4d2e756c36a68e9e2ea224aae0d
dhavalbothra <57654403+dhavalbothra@users.noreply.github.com>
2019-11-17 11:40:46 +0530

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,33 @@
+import time
+from decimal import *
+x=int(input("Enter no. of : "))
+y=input("Enter name of file with .txt")
+getcontext().prec = x+1
+pi=Decimal(3)
+a=time.time()
+def sin(x):
+  sin=Decimal(0)
+  i=Decimal(1)
+  pw=x
+  fact=Decimal(1)
+  sign=Decimal(1)
+  while i<3500:
+    sin += sign*pw/fact
+    i += Decimal(2)
+    fact *= i*(i-1)
+    pw *= Decimal(x**2)
+    sign *= Decimal(-1)
+  return sin
+
+i = 1
+while(i < 50):
+  pi += Decimal(sin(pi))
+  i += 1
+
+f = open(y, "w")
+f.write(str(pi))
+f.close()
+b=time.time()
+print("Time took in seconds are:")
+print(float(b-a))
+s=input()
